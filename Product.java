bc9dc272c19bf1d09318f10e31421c5364486b74
Yoga-Bes <122877797+Yoga-Bes@users.noreply.github.com>
2025-03-01 18:52:44 +0300

    Product.java

--- Product.java
+++ Product.java
@@ -5,6 +5,7 @@ public class Product {
 
     private String name;
     private int productionDate;
+    private String manufacturer;
     private String country;
     private double price;
     private String bookingStatus;
@@ -12,10 +13,11 @@ public class Product {
 
         name = "Стакан";
         productionDate = 2002;
+        manufacturer = "IKEA";
         country = "USA";
         price = 1.5;
         bookingStatus = "В продаже";
-        System.out.println("Имя товара: " + name + "; Год производства: " + productionDate + "; Страна производитель: " + country + "; Цена: " + price + "; Состояние бронирования" + bookingStatus);
+        System.out.println("Имя товара: " + name + "; Год производства: " + productionDate + "; Производитель: " + manufacturer + "; Страна производитель: " + country + "; Цена: " + price + "; Состояние бронирования" + bookingStatus);
 
 
 }
