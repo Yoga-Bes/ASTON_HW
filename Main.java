86388ef169f16c0b8b36d9e59610c4567bdcdd7c
Yoga-Bes <122877797+Yoga-Bes@users.noreply.github.com>
2025-03-02 14:25:42 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,13 +1,39 @@
 public class Main {
 
     public static void main(String[] args) {
-        Park park = new Park("Веселый мир");
 
-        park.addAttraction("Колесо обозрения", "10:00 - 22:00", 300);
-        park.addAttraction(" Американские горки", "12:00 - 20:00", 500);
-        park.addAttraction(" Карусель", "09:00 - 17:00", 200);
+        Dog dog1 = new Dog("Шелдон");
+        Dog dog2 = new Dog("Леонард");
+        Cat cat1 = new Cat("Радж");
+        Cat cat2 = new Cat("Говард");
 
-        park.snowAttractions();
+        dog1.run(100);
+        dog2.run(1100);
+        cat1.run(200);
+        cat2.run(800);
 
+        dog1.swim(5);
+        dog2.swim(20);
+        cat1.swim(15);
+        cat2.swim(0);
+
+        Bowl bowl = new Bowl(30);
+        Cat[] cats = {cat1, cat2};
+        for (Cat cat : cats) {
+            cat.eat(bowl, 20);
+        }
+        for (Cat cat : cats) {
+            System.out.println(cat.name + " сыт: " + cat.isFull());
+        }
+        bowl.addFood(50);
+        for (Cat cat : cats) {
+            cat.eat(bowl, 20);
+        }
+        for (Cat cat : cats) {
+            System.out.println(cat.name + "сыт: " + cat.isFull());
+        }
+        System.out.println("Всего жывотных:" + Animal.getAnimalCount());
+        System.out.println("Всего собак:" + Dog.getDogCount());
+        System.out.println("Всего котов: " + Cat.getCatCount());
     }
 }
